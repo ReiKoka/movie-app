67c276e72560a7be7ebe36ba7af5c4c229f8cd9f
ReiKoka <reikoka1@outlook.com>
2025-01-20 12:27:24 +0100

    DarkMode added to 2nd html file. Created a shared css file for shared code between the two css files

--- js/initSingleMovie.js
+++ js/initSingleMovie.js
@@ -1,7 +1,9 @@
 import { getMovieData, goToMovieById } from "./services/getMovieById.js";
 import { renderSingleMovie } from "./renderSingleMovie.js";
+import { setTheme } from "./setTheme.js";
 
 const initSingleMovie = async () => {
+  setTheme();
   const id = Number(window.location.href.split("?id=")[1]);
   await goToMovieById(id);
   const movie = getMovieData();
